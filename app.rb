e78ee224e3c59a73a15b218a2752806173d182e3
daglus <daglus.ck.ua@gmail.com>
2019-05-02 16:21:15 +0300

    Select comments to @comments variable

--- app.rb
+++ app.rb
@@ -61,6 +61,7 @@ get '/details/:post_id' do
 	results = @db.execute 'select * from Posts where id = ?', [post_id]
 	@row = results[0]
 
+	@comments = @db.execute 'select from Comments where post_id = ? order by id', [post_id]
 	erb :details
 end
 
@@ -82,5 +83,5 @@ post '/details/:post_id' do
 			?
 		)', [content, post_id]
 
-	redirect to('/details/' + post_id) 
+	redirect to('/details/' + post_id)
 end
